9128f8fb23f176a64eaceb598f4b9206b6dbebef
2190342535-code <code@example.com>
2026-04-08 11:35:49 +0800

    修复router路径

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,72 +4,72 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('./views/Home.vue')
+    component: () => import('../views/Home.vue')
   },
   {
     path: '/create-role',
     name: 'CreateRole',
-    component: () => import('./views/CreateRole.vue')
+    component: () => import('../views/CreateRole.vue')
   },
   {
     path: '/mbti-test',
     name: 'MbtiTest',
-    component: () => import('./views/MbtiTest.vue')
+    component: () => import('../views/MbtiTest.vue')
   },
   {
     path: '/mbti-result',
     name: 'MbtiResult',
-    component: () => import('./views/MbtiResult.vue')
+    component: () => import('../views/MbtiResult.vue')
   },
   {
     path: '/astro',
     name: 'AstroAnalysis',
-    component: () => import('./views/AstroAnalysis.vue')
+    component: () => import('../views/AstroAnalysis.vue')
   },
   {
     path: '/daily',
     name: 'DailyLuck',
-    component: () => import('./views/DailyLuck.vue')
+    component: () => import('../views/DailyLuck.vue')
   },
   {
     path: '/fortune',
     name: 'Fortune',
-    component: () => import('./views/Fortune.vue')
+    component: () => import('../views/Fortune.vue')
   },
   {
     path: '/dream',
     name: 'Dream',
-    component: () => import('./views/Dream.vue')
+    component: () => import('../views/Dream.vue')
   },
   {
     path: '/hexagram',
     name: 'Hexagram',
-    component: () => import('./views/Hexagram.vue')
+    component: () => import('../views/Hexagram.vue')
   },
   {
     path: '/chat',
     name: 'Chat',
-    component: () => import('./views/Chat.vue')
+    component: () => import('../views/Chat.vue')
   },
   {
     path: '/life-number',
     name: 'LifeNumber',
-    component: () => import('./views/LifeNumber.vue')
+    component: () => import('../views/LifeNumber.vue')
   },
   {
     path: '/name-analysis',
     name: 'NameAnalysis',
-    component: () => import('./views/NameAnalysis.vue')
+    component: () => import('../views/NameAnalysis.vue')
   },
   {
     path: '/compatibility',
     name: 'Compatibility',
-    component: () => import('./views/Compatibility.vue')
+    component: () => import('../views/Compatibility.vue')
   },
   {
     path: '/profile',
     name: 'Profile',
-    component: () => import('./views/Profile.vue')
+    component: () => import('../views/Profile.vue')
   }
 ]
 
